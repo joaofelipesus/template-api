5d6a73acba8c93b5c7f29630433f750650ada44f
Revelo Devs <devs@revelo.com>
2025-11-07 16:26:18 -0300

    fix: change elasticsearch url

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,5 @@
 # Elasticsearch configuration
 Elasticsearch::Model.client = Elasticsearch::Client.new(
-  host: ENV.fetch('ELASTICSEARCH_URL', 'http://localhost:9200'),
+  host: ENV.fetch('ELASTICSEARCH_URL', 'http://elasticsearch:9200'),
   log: false
 )
