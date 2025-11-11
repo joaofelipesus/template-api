188e6b33794c992199637c3d4c25d5b9c4ad7adf
Revelo Devs <devs@revelo.com>
2025-11-11 10:25:58 -0300

    feat: add students controller

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,41 @@
+class StudentsController < ApplicationController
+before_action :set_student, only: [:show, :update]
+
+  def index
+    @students = Student.all
+
+    render json: @students
+  end
+
+  def show
+    render json: @student
+  end
+
+  def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      render json: @student, status: :created
+    else
+      render json: @student.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @student.update(student_params)
+      render json: @student
+    else
+      render json: @student.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :age)
+  end
+end
